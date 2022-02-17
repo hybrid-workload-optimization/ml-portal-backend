89389d48f0108f23fc33fb289d83efc120650205
as-shpark <as-shpark@kakao.com>
2022-02-17 12:01:46 +0900

    로그 설정 수정

---
+++ src/main/java/kr/co/strato/adapter/k8s/common/service/CommonService.java
@@ -0,0 +1,12 @@
+package kr.co.strato.adapter.k8s.common.service;
+
+import io.fabric8.kubernetes.api.model.HasMetadata;
+
+import java.util.List;
+
+public class CommonService {
+    public List<HasMetadata> parseJson(String json){
+
+        return null;
+    }
+}
