f5f5656b950610f7322ff94cd244ae403378269b
Da-Hyeon Kim <Da-Hyeon Kim@LAPTOP-9P5LA9BS>
2022-02-17 15:14:45 +0900

    import 삭제

--- src/main/java/kr/co/strato/domain/clusterNamespace/service/ClusterNamespaceDomainService.java
+++ src/main/java/kr/co/strato/domain/clusterNamespace/service/ClusterNamespaceDomainService.java
@@ -1,11 +1,7 @@
 package kr.co.strato.domain.clusterNamespace.service;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import kr.co.strato.domain.clusterNode.model.ClusterNode;
-import kr.co.strato.domain.clusterNode.repository.ClusterNodeRepository;
 
 @Service
 public class ClusterNamespaceDomainService {
