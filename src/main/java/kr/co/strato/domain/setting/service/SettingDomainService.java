557e94087e5b552c3e04365c53ee95696a008d28
JoeyNa <JoeyNa@DESKTOP-1F04AIC>
2022-02-22 16:01:53 +0900

    일반세팅 CRUD 추가.

--- src/main/java/kr/co/strato/domain/setting/service/SettingDomainService.java
+++ src/main/java/kr/co/strato/domain/setting/service/SettingDomainService.java
@@ -3,7 +3,13 @@ package kr.co.strato.domain.setting.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import kr.co.strato.domain.setting.model.SettingEntity;
@@ -46,4 +52,25 @@ public class SettingDomainService {
 			throw new InternalServerException(e);
 		}
 	}
+	
+	
+	//간단해보이긴하는데 이거 가독성 구리다고 쓰지 말라고함. getSettings를 하나 만들어서 쓰는게 나아보임.
+	public List<SettingEntity> findSettingEntitiesByType(String settingType){
+		Specification<SettingEntity> spec = SettingEntitySpecs.equalsSettingType(settingType);
+		return settingRepository.findAll(spec);
+	}
+	
+	static class SettingEntitySpecs {
+		@SuppressWarnings("serial")
+		public static Specification<SettingEntity> equalsSettingType(String settingType) {
+			return new Specification<SettingEntity>() {
+				@Override
+				public Predicate toPredicate(Root<SettingEntity> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+					return criteriaBuilder.equal(root.get("settingType"), settingType);
+				}
+			};
+		}
+	}
 }
+
+
