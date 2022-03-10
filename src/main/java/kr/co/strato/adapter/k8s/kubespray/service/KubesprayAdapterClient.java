2b998726019b83fdd69a4b9ff31a6b21056bbbb4
Jae-Youn Kim <Jae-Youn Kim@DESKTOP-1O8VA7B>
2022-03-10 16:29:59 +0900

    kubespray setting get, put api 연동

--- src/main/java/kr/co/strato/adapter/k8s/kubespray/service/KubesprayAdapterClient.java
+++ src/main/java/kr/co/strato/adapter/k8s/kubespray/service/KubesprayAdapterClient.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import kr.co.strato.adapter.k8s.kubespray.model.KubesprayDto;
 
 @FeignClient(name = "kubesprayAdapterClient", url = "${service.cloud-interface.url}")
 public interface KubesprayAdapterClient {
@@ -20,9 +24,19 @@ public interface KubesprayAdapterClient {
 	/**
 	 * k8s kubespray get setting data
 	 * 
-	 * @return array 
+	 * @param String version
+	 * @return String jsonArray
 	 */
 	@GetMapping("/kubespray/setting")
 	public String getKubespraySetting(@RequestParam("version") String version);
 	
+	/**
+	 * k8s kubespray put setting data 
+	 * 
+	 * @param version
+	 * @param data (update object)
+	 * @return boolean 
+	 */
+	@PutMapping("/kubespray/setting")
+	public boolean patchKubespraySetting(@RequestBody KubesprayDto kubesprayDto);
 }
