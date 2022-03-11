c2a59ff9167395a866e11121e01876692ecd6220
yhpark-sptek <yhpark@sptek.co.kr>
2022-03-11 19:39:44 +0900

    Cluster DTO 보완

--- src/main/java/kr/co/strato/portal/cluster/model/ClusterDtoMapper.java
+++ src/main/java/kr/co/strato/portal/cluster/model/ClusterDtoMapper.java
@@ -1,8 +1,15 @@
 package kr.co.strato.portal.cluster.model;
 
+import java.util.HashMap;
+
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import kr.co.strato.domain.cluster.model.ClusterEntity;
 
@@ -13,6 +20,18 @@ public interface ClusterDtoMapper {
 	
 	public ClusterEntity toEntity(ClusterDto dto);
 	
+	//@Mapping(target = "problem",			source = "cluster.problem",		qualifiedByName = "jsonToMap")
 	public ClusterDto toDto(ClusterEntity cluster);
 	
+	@Named("jsonToMap")
+    default HashMap<String, Object> jsonToMap(String json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            HashMap<String, Object> map = mapper.readValue(json, HashMap.class);
+
+            return map;
+        } catch (JsonProcessingException e) {
+            return new HashMap<>();
+        }
+    }
 }
