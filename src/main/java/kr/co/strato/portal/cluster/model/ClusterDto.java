b61de3cbf3073ff7a37bbf883d348e7d8eae000a
yhpark-sptek <yhpark@sptek.co.kr>
2022-03-14 10:35:40 +0900

    Cluster 목록/상세 API 수정 보완

--- src/main/java/kr/co/strato/portal/cluster/model/ClusterDto.java
+++ src/main/java/kr/co/strato/portal/cluster/model/ClusterDto.java
@@ -30,6 +30,38 @@ public class ClusterDto {
 	
 	private String status;
 	
-	//private HashMap<String, Object> problem;
 	
+	@Getter
+	@Setter
+	public static class List {
+		private Long clusterIdx;
+		private String clusterName;
+		private String description;
+		private String status;
+		private int nodeCount;
+		private String provider;
+		private String providerVersion;
+		private String createdAt;
+		private HashMap<String, Object> problem;
+	}
+	
+	@Getter
+	@Setter
+	public static class Detail {
+		private Long clusterIdx;
+		private String clusterName;
+		private String provider;
+		private String providerVersion;
+		private String createdAt;
+		// status
+		private String status;
+		private HashMap<String, Object> problem;
+		// master/worker
+		private float availablePercentMaster;
+		private float availablePercentWorker;
+		private int masterCount;
+		private int workerCount;
+		// move to monitoring service
+		private String monitoringServiceUrl;
+	}
 }
