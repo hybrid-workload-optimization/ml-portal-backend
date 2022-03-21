01237d569bac27108f23719c7f5ba9813e83e6a8
shka <shka@sptek.co.kr>
2022-03-21 22:21:20 +0900

    Login 및 인증용 Interceptor , Controller 생성

--- src/main/java/kr/co/strato/portal/common/controller/AuthController.java
+++ src/main/java/kr/co/strato/portal/common/controller/AuthController.java
@@ -1,24 +1,40 @@
 package kr.co.strato.portal.common.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.strato.portal.common.service.AuthService;
 
 @RestController
+@RequestMapping("/api/v1/access-manage")
 public class AuthController {
 	
 	@Autowired
 	AuthService authService;
 	
 	//token 요청(로그인)
-	
+	@PostMapping("/login")
+	public void doLogin() {
+		System.out.println("로그인");
+	}
 	
 	//token refresh 요청
-	
+	@GetMapping("/token-refresh")
+	public void tokenRefresh() {
+		System.out.println("token refresh..");
+	}
 	
 	//token 유효성 검증
-
+	public void tokenVerify() {
+		System.out.println("토큰 검증..");
+	}
 	
-	//token 삭제 요청
+	
+	//token 삭제 요청(로그아웃)
+	public void doLogout() {
+		System.out.println("do Logout..");
+	}
 }
--- src/main/java/kr/co/strato/portal/common/service/AuthService.java
+++ src/main/java/kr/co/strato/portal/common/service/AuthService.java
@@ -1,19 +1,36 @@
 package kr.co.strato.portal.common.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kr.co.strato.global.util.KeyCloakApiUtil;
 
 @Service
 public class AuthService {
 	
-	//token 요청(로그인)
+	@Autowired
+	KeyCloakApiUtil keyCloakApiUtil;
 	
+	//token 요청(로그인)
+	public void doLogin() {
+		System.out.println("로그인");
+//		keyCloakApiUtil.getTokenByUser(null);
+	}
 	
 	//token refresh 요청
-	
+	public void tokenRefresh() {
+		System.out.println("token refresh..");
+	}
 	
 	//token 유효성 검증
-
+	public void tokenVerify() {
+		System.out.println("토큰 검증..");
+	}
 	
-	//token 삭제 요청
+	
+	//token 삭제 요청(로그아웃)
+	public void doLogout() {
+		System.out.println("do Logout..");
+	}
 
 }
