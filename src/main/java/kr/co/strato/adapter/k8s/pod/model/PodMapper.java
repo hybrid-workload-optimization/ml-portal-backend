2ca411105f2c1d5935630c3594e63d969d26e27e
yeonju <yjgim@sptek.co.kr>
2022-03-23 13:50:47 +0900

    오타 수정

--- src/main/java/kr/co/strato/adapter/k8s/pod/model/PodMapper.java
+++ src/main/java/kr/co/strato/adapter/k8s/pod/model/PodMapper.java
@@ -95,7 +95,7 @@ public interface PodMapper {
     				.filter(quantity -> quantity != null)
     				.map(map -> {
     					map.setAmount(map.getAmount().replaceAll("[^0-9]", ""));
-    					map.setFormat(map.getFormat().replaceAll("[^0-9]", ""));
+    					map.setFormat(map.getFormat().replaceAll("[^a-zA-Z]", ""));
     					return map;
     				})
     				.collect(Collectors.toList());	
@@ -106,7 +106,7 @@ public interface PodMapper {
     				.filter(quantity -> quantity != null)
     				.map(map -> {
     					map.setAmount(map.getAmount().replaceAll("[^0-9]", ""));
-    					map.setFormat(map.getFormat().replaceAll("[^0-9]", ""));
+    					map.setFormat(map.getFormat().replaceAll("[^a-zA-Z]", ""));
     					return map;
     				})
     				.collect(Collectors.toList());
