61cb74ee467ed1d35702fda571006fe578904f79
hclee <hclee@sptek.co.kr>
2022-03-24 14:39:21 +0900

    Project count 로직 수정

--- src/main/java/kr/co/strato/portal/dashboard/service/DashboardService.java
+++ src/main/java/kr/co/strato/portal/dashboard/service/DashboardService.java
@@ -18,6 +18,8 @@ import kr.co.strato.adapter.k8s.pod.service.PodAdapterService;
 import kr.co.strato.domain.cluster.model.ClusterEntity;
 import kr.co.strato.domain.cluster.service.ClusterDomainService;
 import kr.co.strato.domain.node.model.NodeEntity;
+import kr.co.strato.domain.project.model.ProjectEntity;
+import kr.co.strato.domain.project.service.ProjectDomainService;
 import kr.co.strato.portal.cluster.model.ClusterNodeDto;
 import kr.co.strato.portal.cluster.model.ClusterNodeDtoMapper;
 import kr.co.strato.portal.cluster.service.ClusterNodeService;
@@ -36,6 +38,9 @@ public class DashboardService {
     private ClusterDomainService clusterDomainService;
 	
 	@Autowired
+	private ProjectDomainService projectDomainService;
+	
+	@Autowired
 	private NodeAdapterService nodeAdapterService;
 	
 	@Autowired
@@ -43,7 +48,14 @@ public class DashboardService {
 	
 	@Autowired
     private PodAdapterService podAdapterService;
-
+	
+	
+	/**
+	 * System admin용 Dashboard 데이터 반환.
+	 * @param projectIdx
+	 * @param clusterIdx
+	 * @return
+	 */
 	public DashboardSystemAdminDto getDashboardInfoForSystemAdmin(Long projectIdx, Long clusterIdx) {
 		int projectCount = 0;
 		int clusterCount = 0;
@@ -53,85 +65,94 @@ public class DashboardService {
 		int workerCount = 0;		
 		int masterReadyCount = 0;
 		int workerReadyCount = 0;		
-		int restartWithinTenMinutes = 0;		
+		int restartWithinTenMinutes = 0;	
+		
+		int readyCount = 0;
+		int totalUtilization = 0;
+		int masterUtilization = 0;
+		int workerUtilization = 0;
+		
+		String nodeUtilizationState = null;
 		
 		List<NodeEntity> nodeEntitys = new ArrayList<>();
 		if(projectIdx == null) {
 			projectCount = selectService.getSelectProjects().size();		
-		} else {
-			projectCount = 1;	
+		} else {			
+			if(projectDomainService.getProjectById(projectIdx).orElse(null) != null) {
+				projectCount = 1;	
+			}
 		}
 		
 		List<ClusterNodeDto.ResListDto> list = new ArrayList<>();
 		List<ClusterEntity> clusters = getKubeConfigIds(projectIdx, clusterIdx);
 		clusterCount = clusters.size();
 		
-		long currentTime = new Date().getTime();
-		for(ClusterEntity cluster: clusters) {
-			Long kubeConfigId = cluster.getClusterId();
-			String clusterName = cluster.getClusterName();
-			try {
-				List<Node> nodes = nodeAdapterService.getNodeList(kubeConfigId);
-				for(Node node : nodes) {
-					NodeEntity entity = nodeService.toEntity(node, cluster.getClusterIdx());
-					nodeEntitys.add(entity);
-					
-					List<Pod> k8sPods = podAdapterService.getList(kubeConfigId, entity.getName(), null, null, null);
-					long runningSize = k8sPods.stream().filter(p -> p.getStatus().getPhase().equals("Running")).count();
-					int podCount = k8sPods.size();
-					
-					String podStatus = String.format("%d/%d", runningSize, podCount);
-		        	
-		        	
-		        	ClusterNodeDto.ResListDto nodeListDto = ClusterNodeDtoMapper.INSTANCE.toResListDto(entity);
-		        	nodeListDto.setClusterName(clusterName);
-		        	nodeListDto.setPodStatus(podStatus);
-		        	list.add(nodeListDto);
-		        	
-					
-					boolean isReady = Boolean.parseBoolean(entity.getStatus());				
-					String role = entity.getRole();
-					if(role.toLowerCase().contains("master")) {
-						masterCount++;
-						if(isReady) {
-							masterReadyCount++;
+		if(clusterCount > 0) {
+			long currentTime = new Date().getTime();
+			for(ClusterEntity cluster: clusters) {
+				Long kubeConfigId = cluster.getClusterId();
+				String clusterName = cluster.getClusterName();
+				try {
+					List<Node> nodes = nodeAdapterService.getNodeList(kubeConfigId);
+					for(Node node : nodes) {
+						NodeEntity entity = nodeService.toEntity(node, cluster.getClusterIdx());
+						nodeEntitys.add(entity);
+						
+						List<Pod> k8sPods = podAdapterService.getList(kubeConfigId, entity.getName(), null, null, null);
+						long runningSize = k8sPods.stream().filter(p -> p.getStatus().getPhase().equals("Running")).count();
+						int podCount = k8sPods.size();
+						
+						String podStatus = String.format("%d/%d", runningSize, podCount);
+			        	
+			        	
+			        	ClusterNodeDto.ResListDto nodeListDto = ClusterNodeDtoMapper.INSTANCE.toResListDto(entity);
+			        	nodeListDto.setClusterName(clusterName);
+			        	nodeListDto.setPodStatus(podStatus);
+			        	list.add(nodeListDto);
+			        	
+						
+						boolean isReady = Boolean.parseBoolean(entity.getStatus());				
+						String role = entity.getRole();
+						if(role.toLowerCase().contains("master")) {
+							masterCount++;
+							if(isReady) {
+								masterReadyCount++;
+							}
+						} else {
+							workerCount++;
+							if(isReady) {
+								workerReadyCount++;
+							}
 						}
-					} else {
-						workerCount++;
-						if(isReady) {
-							workerReadyCount++;
+						
+						if(withinTenMinutes(node, currentTime)) {
+							//10분 이내 재시작 노드
+							restartWithinTenMinutes++;
 						}
-					}
-					
-					if(withinTenMinutes(node, currentTime)) {
-						//10분 이내 재시작 노드
-						restartWithinTenMinutes++;
-					}
-				}			
-				nodeCount+=nodes.size();
-			} catch (Exception e) {
-				log.error(e.getMessage(), e);
+					}			
+					nodeCount+=nodes.size();
+				} catch (Exception e) {
+					log.error(e.getMessage(), e);
+				}
+				
 			}
 			
-		}
-		
-		int readyCount = masterReadyCount + workerReadyCount;
-		
-		int totalUtilization = Long.valueOf(Math.round((double) readyCount / (double) nodeCount * 100)).intValue();
-		int masterUtilization = Long.valueOf(Math.round((double) masterReadyCount / (double) masterCount * 100)).intValue();
-		int workerUtilization = Long.valueOf(Math.round((double) workerReadyCount / (double) workerCount * 100)).intValue();
-		
-		String nodeUtilizationState = null;
-		
-		//90 이상 Good
-		//90 미만 70 이상 Warning
-		//70 미만 Bad
-		if(totalUtilization >= 90) {
-			nodeUtilizationState = "Good";
-		} else if(totalUtilization > 70) {
-			nodeUtilizationState = "Warning";
-		} else {
-			nodeUtilizationState = "Bad";
+			readyCount = masterReadyCount + workerReadyCount;
+			
+			totalUtilization = Long.valueOf(Math.round((double) readyCount / (double) nodeCount * 100)).intValue();
+			masterUtilization = Long.valueOf(Math.round((double) masterReadyCount / (double) masterCount * 100)).intValue();
+			workerUtilization = Long.valueOf(Math.round((double) workerReadyCount / (double) workerCount * 100)).intValue();
+			
+			//90 이상 Good
+			//90 미만 70 이상 Warning
+			//70 미만 Bad
+			if(totalUtilization >= 90) {
+				nodeUtilizationState = "Good";
+			} else if(totalUtilization > 70) {
+				nodeUtilizationState = "Warning";
+			} else {
+				nodeUtilizationState = "Bad";
+			}
 		}
 		
 		return DashboardSystemAdminDto.builder()
