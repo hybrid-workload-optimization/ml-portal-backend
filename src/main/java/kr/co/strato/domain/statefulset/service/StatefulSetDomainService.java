dfc74d1e6b32c7ab52663519ed4eb0d62b4f1449
as-shpark <as-shpark@kakao.com>
2022-03-29 09:21:21 +0900

    스테이트풀셋 생성 오류 수정

--- src/main/java/kr/co/strato/domain/statefulset/service/StatefulSetDomainService.java
+++ src/main/java/kr/co/strato/domain/statefulset/service/StatefulSetDomainService.java
@@ -39,7 +39,7 @@ public class StatefulSetDomainService {
         }
         ClusterEntity cluster = ClusterEntity.builder().clusterIdx(clusterIdx).build();
         NamespaceEntity namespace = getNamespace(cluster, namespaceName);
-        if(namespace != null){
+        if(namespace == null){
             throw new NotFoundResourceException("namespace가 존재하지 않습니다.");
         }
         statefulSet.setNamespace(namespace);
