1ada25f11f8b2cb048116820005dfd0cad6d76b4
hclee <hclee@sptek.co.kr>
2022-04-14 07:56:46 +0900

    ...

--- src/main/java/kr/co/strato/portal/addon/service/AddonService.java
+++ src/main/java/kr/co/strato/portal/addon/service/AddonService.java
@@ -9,7 +9,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
--- src/main/java/kr/co/strato/portal/dashboard/controller/DashboardController.java
+++ src/main/java/kr/co/strato/portal/dashboard/controller/DashboardController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import kr.co.strato.domain.node.model.NodeEntity;
 import kr.co.strato.global.model.ResponseWrapper;
 import kr.co.strato.portal.cluster.model.ClusterNodeDto.ResListDetailDto;
 import kr.co.strato.portal.dashboard.model.SystemAdminNodeStateDto;
