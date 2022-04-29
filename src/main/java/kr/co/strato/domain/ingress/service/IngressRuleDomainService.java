518c2a11d7758ed7fdadef22014643eff237f1f0
hclee <hclee@sptek.co.kr>
2022-04-29 10:56:36 +0900

    필요없는 import 구문 제거

--- src/main/java/kr/co/strato/domain/ingress/service/IngressRuleDomainService.java
+++ src/main/java/kr/co/strato/domain/ingress/service/IngressRuleDomainService.java
@@ -2,14 +2,12 @@ package kr.co.strato.domain.ingress.service;
 
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.strato.domain.IngressController.model.IngressControllerEntity;
 import kr.co.strato.domain.IngressController.repository.IngressControllerRepository;
-import kr.co.strato.domain.ingress.model.IngressEntity;
 import kr.co.strato.domain.ingress.model.IngressRuleEntity;
 import kr.co.strato.domain.ingress.repository.IngressRuleRepository;
 import kr.co.strato.global.error.exception.InternalServerException;
