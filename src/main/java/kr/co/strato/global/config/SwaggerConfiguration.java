27a57c968d7fb124841757d3e317bc991afa58a2
hclee <hclee@sptek.co.kr>
2022-08-04 10:12:09 +0900

    ...

--- src/main/java/kr/co/strato/global/config/SwaggerConfiguration.java
+++ src/main/java/kr/co/strato/global/config/SwaggerConfiguration.java
@@ -14,8 +14,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-//@Configuration
-//@EnableSwagger2
+@Configuration
 public class SwaggerConfiguration {
 
 	public static final String AUTHORIZATION_HEADER = "Authorization";
