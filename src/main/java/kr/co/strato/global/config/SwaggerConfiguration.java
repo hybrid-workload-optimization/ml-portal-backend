c242b8d3b27afbc3ec77395ce5f134595dcfa975
hclee <hclee@sptek.co.kr>
2022-08-04 09:47:40 +0900

    ...

--- src/main/java/kr/co/strato/global/config/SwaggerConfiguration.java
+++ src/main/java/kr/co/strato/global/config/SwaggerConfiguration.java
@@ -8,12 +8,14 @@ import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfiguration {
 
 	public static final String AUTHORIZATION_HEADER = "Authorization";
@@ -30,10 +32,15 @@ public class SwaggerConfiguration {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.OAS_30).apiInfo(apiInfo())
-				.securityContexts(Arrays.asList(securityContext())).securitySchemes(Arrays.asList(apiKey())).select()
+		return new Docket(DocumentationType.OAS_30)
+				.apiInfo(apiInfo())
+				.securityContexts(Arrays.asList(securityContext()))
+				.securitySchemes(Arrays.asList(apiKey()))
+				.select()
 				.apis(RequestHandlerSelectors.basePackage("kr.co.strato.portal.machineLearning"))
-				.paths(PathSelectors.any()).build();
+				.paths(PathSelectors.any())
+				.build()
+				.useDefaultResponseMessages(false);
 	}
 
 	private ApiKey apiKey() {
