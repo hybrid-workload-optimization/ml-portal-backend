432755ab180b2292d79be606ac616fe943fdf4cc
hclee <hclee@sptek.co.kr>
2022-08-08 10:43:51 +0900

    prometheus url 변경

--- src/main/java/kr/co/strato/portal/machineLearning/service/MLInterfaceAPIService.java
+++ src/main/java/kr/co/strato/portal/machineLearning/service/MLInterfaceAPIService.java
@@ -253,7 +253,7 @@ public class MLInterfaceAPIService {
 	}
 	
 	public String getPrometheusUrl(Long clusterId) {
-		String url = "http://210.217.178.117:30015/";
+		String url = "http://210.217.178.114:30015/";
 		return url;
 	}
 	
