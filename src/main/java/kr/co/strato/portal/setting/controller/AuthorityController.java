523c29425bc046526b694c5c92844cb280c4e98b
hclee <hclee@sptek.co.kr>
2023-03-20 11:03:42 +0900

    ....

--- src/main/java/kr/co/strato/portal/setting/controller/AuthorityController.java
+++ src/main/java/kr/co/strato/portal/setting/controller/AuthorityController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -72,7 +73,7 @@ public class AuthorityController extends CommonController {
 	}
 	
 	//권한 수정
-	@PatchMapping("/authorities")
+	@PutMapping("/authorities")
 	public ResponseWrapper<Long> patchAuthority(@RequestBody AuthorityRequestDto.ReqModifyDto param) {
 		Long idx = authorityService.modifyUserRole(param);
 		return new ResponseWrapper<>(idx);
