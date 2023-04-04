024cb18571605df1aff75b4c21dc743d712bcbf8
hclee <hclee@localhost>
2023-04-04 14:18:33 +0900

    Eureka 설정 제거

--- src/main/java/kr/co/strato/PaasPortalApplication.java
+++ src/main/java/kr/co/strato/PaasPortalApplication.java
@@ -2,10 +2,8 @@ package kr.co.strato;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 public class PaasPortalApplication {
