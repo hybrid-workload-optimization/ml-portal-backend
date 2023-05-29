87f5eb9c23b2e0b5228bec6b1fd4c7e7a988a529
hclee <hclee@172.16.11.96>
2023-05-30 08:38:30 +0900

    ...

--- src/main/java/kr/co/strato/portal/ml/service/MLClusterAPIAsyncService.java
+++ src/main/java/kr/co/strato/portal/ml/service/MLClusterAPIAsyncService.java
@@ -138,7 +138,18 @@ public class MLClusterAPIAsyncService {
 		return ArgoCDInfo.builder().url(url).password(password).build();
 	}
 	
-	public String getExternalUrl(ClusterEntity cluster) {		
+	public String getExternalUrl(ClusterEntity cluster) {
+		String externalUrl = null;
+		if(cluster.getClusterName().equals("vsphere-cluster-demo")) {
+			externalUrl = "10.10.20.180:30007";
+		} else if(cluster.getClusterName().equals("vsphere-cluster-create")) {
+			externalUrl = "10.10.20.185:30007"; 
+		}
+		
+		if(externalUrl != null) {
+			return externalUrl;
+		}
+		
 		if(cluster.getProvider().toLowerCase().equals("kubernetes")
 				|| cluster.getProvider().toLowerCase().equals("vmware")
 				|| cluster.getProvider().toLowerCase().equals("openstack")) {
