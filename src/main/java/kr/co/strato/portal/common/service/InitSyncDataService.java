8fc1468bd695801fe9590e491608dfd5435aa0b9
ymkwon <ymkwon@strato.co.kr>
2023-05-31 16:14:18 +0900

    test

--- src/main/java/kr/co/strato/portal/common/service/InitSyncDataService.java
+++ src/main/java/kr/co/strato/portal/common/service/InitSyncDataService.java
@@ -81,7 +81,7 @@ public class InitSyncDataService {
 	
 					Long resultIdx = authorityService.postUserRole(role);
 					
-					log.info("result idx: {}", resultIdx);
+					log.info("result iddx: {}", resultIdx);
 
 				}
 				
