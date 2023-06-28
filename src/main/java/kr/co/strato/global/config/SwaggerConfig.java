ccf79838dcc94bd36d87e6e80707a5a28fd9eafc
hclee <hclee@172.16.11.96>
2023-06-28 15:54:38 +0900

    ....

--- src/main/java/kr/co/strato/global/config/SwaggerConfig.java
+++ src/main/java/kr/co/strato/global/config/SwaggerConfig.java
@@ -7,15 +7,17 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-//@Configuration
+@Configuration
 public class SwaggerConfig {
 
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.OAS_30)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("kr.co.strato.portal"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+        
+        
     }
 }
